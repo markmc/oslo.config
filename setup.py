7e54a17613b8099378d7d1338ef032eb99328b89
Mark McLoughlin <markmc@redhat.com>
2013-03-18 07:55:40 +0000

    Switch to oslo.packaging
    Use the declarative metadata style in setup.cfg enabled by d2to1.
    
    We can remove the setuptools code under magic/ imported from
    oslo-incubator.
    
    Change-Id: I83a1e9224185ad547bdb333feaf81419c24f768b

--- setup.py
+++ setup.py
@@ -17,36 +17,6 @@
 
 import setuptools
 
-from magic.openstack.common import setup
-
-package = 'oslo.config'
-version = '1.2.0'
-
-requires = setup.parse_requirements()
-depend_links = setup.parse_dependency_links()
-tests_require = setup.parse_requirements(['tools/test-requires'])
-
-
 setuptools.setup(
-    name=package,
-    version=setup.get_version(package, version),
-    description='Oslo configuration API',
-    long_description='The Oslo configuration API supports parsing command '
-                     'line arguments and .ini style configuration files.',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: OpenStack',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6', ],
-    author='OpenStack',
-    author_email='openstack-dev@lists.openstack.org',
-    url='http://www.openstack.org/',
-    license='Apache Software License',
-    packages=['oslo', 'oslo.config'],
-    namespace_packages=['oslo'],
-    cmdclass=setup.get_cmdclass(),
-    install_requires=requires,
-    tests_require=tests_require,
-    dependency_links=depend_links,
-)
+    setup_requires=['d2to1', 'oslo.packaging'],
+    d2to1=True)
